7dba00107d9357aa2bda80f3a93252bbc5f92562
DuanYi <2023200660@ruc.edu.cn>
2024-10-21 11:02:27 +0800

    10.21解决羽毛球问题

--- C_Program_of_YOJ/170.c
+++ C_Program_of_YOJ/170.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main(){
     int ld=0,lzw=0;
-    while ( !feof(stdin) ){
+    while (1){
         int a;
-        scanf("%d",&a);
         if(a==0){ld+=1;}
         else{lzw+=1;}
-        if(ld>=21||lzw>=21){
-            if(ld-lzw>=1){printf("0");break;}
-            else if(lzw-ld>=1){printf("0");break;}
+        if(ld>=21&& ld-lzw>=1){printf("0");break;}
+        if(lzw>=21&& lzw-ld>=1){printf("1");break;}
+        if(ld>=21||lzw>21){
+            if(ld-lzw>1){printf("0");break;}
+            else if(lzw-ld>1){printf("1");break;}
         }
     }
     return 0;
